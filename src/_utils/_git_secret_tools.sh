fc1992ef7baea6e98d59e0716d8d8e57677d41e9
joshr <joshr>
2018-07-03 08:16:57 -0400

    updates

--- src/_utils/_git_secret_tools.sh
+++ src/_utils/_git_secret_tools.sh
@@ -210,6 +210,7 @@ function _gawk_inplace {
   local parms="$*"
   local dest_file
   dest_file="$(echo "$parms" | gawk -v RS="'" -v FS="'" 'END{ gsub(/^\s+/,""); print $1 }')"
+  # above doesn't handle files with spaces correctly
 
   _temporary_file
 
